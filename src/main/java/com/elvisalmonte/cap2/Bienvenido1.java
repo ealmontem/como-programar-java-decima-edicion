4408704bdca27750d58736f7fbfe04886e619fe6
Elvis Almonte <elvisalmonte14@gmail.com>
2025-01-24 21:42:53 -0400

    Creacion primera clase

---
+++ src/main/java/com/elvisalmonte/cap2/Bienvenido1.java
@@ -0,0 +1,13 @@
+package com.elvisalmonte.cap2;
+
+/** Introducción a las aplicaciones
+ * en Java: entrada/salida
+ * 2 y operadores
+ **/
+
+public class Bienvenido1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
