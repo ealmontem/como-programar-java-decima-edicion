41bd7c697e98b9e5b2318d86984247c58af6b0b3
Elvis Almonte <elvisalmonte14@gmail.com>
2025-01-24 23:51:26 -0400

    Lambdas y fl ujos de Java SE 8

--- src/main/java/com/elvisalmonte/cap17/OperacionesIntStream.java
+++ src/main/java/com/elvisalmonte/cap17/OperacionesIntStream.java
@@ -5,11 +5,16 @@ import java.util.stream.IntStream;
 public class OperacionesIntStream {
     public static void main(String[] args) {
         int[] valores = {3, 10, 6, 1, 4, 8, 2, 5, 9, 7};
+
         //Muestra valores originales
+
         System.out.println("Muestra los valores originales");
         IntStream.of(valores)
-                .forEach(valor -> System.out.printf("%d",valor));
+                .forEach(System.out::println);
+
         System.out.println();
+
+
     }
 
 }
